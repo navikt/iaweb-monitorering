cbfe6ebf22656776aa77a589eb1063c216a2f481
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-10 14:33:34 +0200

    Sjekk status på iawebnav hvert tiende sekund

---
+++ src/metrikker.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const apiMetrics = require('prometheus-api-metrics');
+const Prometheus = require('prom-client');
+
+const miljøer = ['t1', 'q1', 'q0', 'p'];
+
+const iawebnavQ1 = new Prometheus.Gauge({
+    name: 'iawebnavQ1',
+    help: 'iaweb nav q1'
+});
+
+const oppdaterMetrikker = (antallMillisekunderMellomHverOppdatering) => setInterval(() => {
+    console.log('Oppdaterer status til iawebnav i q1...');
+    returnererEndepunkt200OK(urlTilIawebnav('q1')).then(erOppe => {
+        console.log('Status til iawebnav i q1: ' + erOppe);
+        iawebnavQ1.set(erOppe);
+    });
+}, antallMillisekunderMellomHverOppdatering);
+
+const hentMetrikker = async () => {
+    const metrikker = {};
+    await Promise.all(miljøer.map(async (miljø) => {
+        metrikker['iawebnav' + hentMiljøUrlStreng(miljø)] = await returnererEndepunkt200OK(urlTilIawebnav(miljø));
+    }));
+    return metrikker;
+};
+
+const urlTilIawebnav = miljø => {
+    return `https://itjenester${hentMiljøUrlStreng(miljø)}.oera.no/iaweb/internal/selftest.json`;
+};
+
+const hentMiljøUrlStreng = miljø => {
+    if (miljø === 'p') {
+        return '';
+    } else {
+        return '-' + miljø;
+    }
+};
+
+const returnererEndepunkt200OK = async (url) => {
+    return await axios.get('https://arbeidsgiver-q.nav.no/kontakt-oss/internal/isAslive').then(res => {
+        if (res.status === 200) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+};
+
+module.exports = {oppdaterMetrikker, hentMetrikker};
--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,28 @@
+const {oppdaterMetrikker, hentMetrikker } = require('../metrikker');
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const apiMetrics = require('prometheus-api-metrics');
 
 const BASE_PATH = '/iaweb-monitorering';
 const PORT = 8080;
 
+oppdaterMetrikker(10000);
+
 const startServer = () => {
     app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
     app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
+
+    app.get(`${BASE_PATH}/internal/metrikker`, (req, res) => {
+        hentMetrikker().then(metrikker => {
+            res.send(metrikker);
+        });
+    });
+
+    app.use(
+        apiMetrics({
+            metricsPath: BASE_PATH + '/internal/metrics',
+        })
+    );
 
     app.get(`${BASE_PATH}/selftest/iawebnav/q1`, (req, res) => {
         axios.get('https://itjenester-q1.oera.no/iaweb/internal/selftest.json').then(selftestRes => {
@@ -15,14 +30,16 @@ const startServer = () => {
         });
     });
 
-    /*
     app.get(`${BASE_PATH}/selftest`, (req, res) => {
         axios.get('https://arbeidsgiver-q.nav.no/kontakt-oss/internal/isAlive').then(selftestRes => {
-            //console.log(res2.status);
+            if (selftestRes.status === 200) {
+                iawebnavQ1.set(1);
+            } else {
+                iawebnavQ1.set(0);
+            }
             res.sendStatus(selftestRes.status);
         });
     });
-    */
 
     app.listen(PORT, () => {
         console.log('Server listening on port', PORT);
