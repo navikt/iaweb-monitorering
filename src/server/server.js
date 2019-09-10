b69f07e85f6b0d2a84bccc3e63dbd568fbeb0619
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-10 12:42:45 +0200

    Fjern basepath

--- src/server/server.js
+++ src/server/server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 
-const BASE_PATH = '/iaweb-monitorering';
 const PORT = 8080;
 
 const startServer = () => {
-    app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
-    app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
+    app.get(`/internal/isAlive`, (req, res) => res.sendStatus(200));
+    app.get(`/internal/isReady`, (req, res) => res.sendStatus(200));
 
-    app.get(`${BASE_PATH}/selftest/iawebnav/q1`, (req, res) => {
+    app.get(`/selftest/iawebnav/q1`, (req, res) => {
         axios.get('https://itjenester-q1.oera.no/iaweb/internal/selftest.json').then(selftestRes => {
             res.sendStatus(selftestRes.status);
         });
