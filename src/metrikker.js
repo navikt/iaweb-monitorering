0d45094bf0ee52ab089b7604b2ca19468823e432
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-09-11 14:06:19 +0200

    logg mer

--- src/metrikker.js
+++ src/metrikker.js
@@ -58,9 +58,7 @@ const tolkResultatForIawebSolr = selftestResultat => {
 const hentSelftestResultat = async (app, miljø) => {
     const url = urlTilApp(app, miljø);
     try {
-        const selftestResultat = await axios.get(url, {
-            maxRedirects: 100 // html-selftestene redirecter kallet til å inneholde et løpenummer.
-        });
+        const selftestResultat = await axios.get(url);
         if (app === 'iawebsolr') {
             return tolkResultatForIawebSolr(selftestResultat);
         }
@@ -72,7 +70,7 @@ const hentSelftestResultat = async (app, miljø) => {
     } catch (error) {
         return {
             status: 'kall feilet',
-            data: error.message,
+            data: error,
             url: url,
         };
     }
